7c4262c840349a9f29038edcaf25259f7512d5ef
rasca0027 <rasca0027@gmail.com>
2015-03-10 17:34:16 +0800

    first commit

---
+++ facebook_login/utils.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+import requests
+
+def get_token():
+    
+    with open('token.txt', 'r') as file:
+        token = file.read()   
+    return token
+
+def login():                                                                                                                                  
+
+    APP_ID = settings.APP_ID
+    APP_SECRET = settings.APP_SECRET
+    REDIRECT_URL = settings.REDIRECT_URL
+
+    url = 'https://www.facebook.com/dialog/oauth?client_id=' + APP_ID + '&redirect_uri=' + REDIRECT_URL + '&scope=ads_management'
+    print url
+  
+    code = raw_input('enter the code: ')
+    url2 = 'https://graph.facebook.com/v2.2/oauth/access_token?client_id=' + APP_ID + '&redirect_uri=' + REDIRECT_URL + '&client_secret=' + APP_SECRET + '&code=' + code
+  
+    r = requests.get(url2)
+    access_token = r.text[13:]
+  
+    # store access token
+    with open('token.txt', 'w') as file:
+        file.write(access_token)
+
+    return access_token
+
