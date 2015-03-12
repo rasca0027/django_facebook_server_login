9e3ea6725390dcf2b0f8904c92d76cf42138e085
rasca0027 <rasca0027@gmail.com>
2015-03-12 17:01:04 +0800

    add name to urls

--- facebook_login/urls.py
+++ facebook_login/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from facebook_login import views
 
 urlpatterns = patterns('',
-    url(r'^login/$', 'facebook_login.views.login'),
-    url(r'^login_success/', 'facebook_login.views.login_success')
+    url(r'^login/$', 'facebook_login.views.login', name='login_page'),
+    url(r'^login_success/', 'facebook_login.views.login_success', name='login_success_page')
     )
