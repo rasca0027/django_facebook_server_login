a3e2c792ff2be29a677d6535633ae529e6a2cee4
rasca0027 <rasca0027@gmail.com>
2015-03-17 16:29:07 +0800

    clean repo

--- build/lib.linux-x86_64-2.7/facebook_login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from facebook_login import views
-
-urlpatterns = patterns('',
-    url(r'^login/$', 'facebook_login.views.login', name='login_page'),
-    url(r'^login_success/', 'facebook_login.views.login_success', name='login_success_page')
-    )
